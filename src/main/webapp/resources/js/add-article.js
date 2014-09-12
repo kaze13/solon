c12466636d327edae990ee0538ab086863e63a97
zhang_t <zhang_t@worksap.co.jp>
2014-09-13 01:20:19 +0800

    file server

--- src/main/webapp/resources/js/add-article.js
+++ src/main/webapp/resources/js/add-article.js
@@ -1,6 +1,30 @@
 $(document).ready(function(){
 	
-	tinymce.init({
-	    selector: "textarea"
-	});
+	//document.getElementById( 'count' ).innerHTML = window.CKEDITOR_LANGS.length;
+
+	var editor;
+
+	function createEditor( languageCode ) {
+		if ( editor )
+			editor.destroy();
+
+		// Replace the <textarea id="editor"> with an CKEditor
+		// instance, using default configurations.
+		editor = CKEDITOR.replace( 'editor1', {
+			language: languageCode,
+
+			on: {
+				instanceReady: function() {
+					// Wait for the editor to be ready to set
+					// the language combo.
+				
+				}
+			}
+		});
+	}
+
+
+	// At page startup, load the default language:
+	createEditor( '' );
+
 });
