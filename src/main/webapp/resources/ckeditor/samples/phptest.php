ca1f00ab8b17ef7b56e152ed06f10bc2f2761e46
zhang_t <zhang_t@worksap.co.jp>
2014-09-13 01:23:07 +0800

    file server

--- src/main/webapp/resources/ckeditor/samples/phptest.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (extension_loaded('gd') && function_exists('gd_info')) {
-    echo "PHP GD library is installed on your web server";
-}
-else {
-    echo "PHP GD library is NOT installed on your web server";
-}
-?>
